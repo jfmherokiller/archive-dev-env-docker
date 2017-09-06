97400580a1f33efa21b4f2172f60f11b5c13f28b
NoahTheGoodra <peterpan0413@live.com>
2017-09-05 19:20:17 -0500

    made a mistake with config
    Signed-off-by: NoahTheGoodra <peterpan0413@live.com>

--- redis_server.sh
+++ redis_server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # `/sbin/setuser www-data` runs the given command as the user `www-data`.
 # If you omit that part, the command will be run as root.
-exec /sbin/setuser www-data /usr/local/bin/redis-server /etc/redis/6379.conf >>/var/log/redis.log 2>&1
+exec /sbin/setuser www-data /usr/local/bin/redis-server /etc/redis/redis.conf >>/var/log/redis.log 2>&1
